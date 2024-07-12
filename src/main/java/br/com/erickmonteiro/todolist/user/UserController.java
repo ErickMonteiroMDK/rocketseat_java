e6a44c4331649f7a6af70b8028402b54fdc9810e
ErickMonteiroMDK <erickmonteiromdk@gmail.com>
2024-07-12 18:48:12 -0300

    Feat: Creating new database with H2db

--- src/main/java/br/com/erickmonteiro/todolist/user/UserController.java
+++ src/main/java/br/com/erickmonteiro/todolist/user/UserController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+        System.out.println(userModel.getName());
     }
 
 }
--- src/main/java/br/com/erickmonteiro/todolist/user/UserModel.java
+++ src/main/java/br/com/erickmonteiro/todolist/user/UserModel.java
@@ -1,8 +1,28 @@
 package br.com.erickmonteiro.todolist.user;
 
+import org.hibernate.annotatios.CreationTimestamp;
+
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import java.util.UUID;
+
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
 }
