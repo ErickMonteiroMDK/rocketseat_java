48474ae7f09b1435923c94d959ad2dd06ccb248c
ErickMonteiroMDK <erickmonteiromdk@gmail.com>
2024-07-13 14:53:05 -0300

    feat: creating controller and repository for func Task

--- src/main/java/br/com/erickmonteiro/todolist/task/TaskModel.java
+++ src/main/java/br/com/erickmonteiro/todolist/task/TaskModel.java
@@ -2,13 +2,14 @@ package br.com.erickmonteiro.todolist.task;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-import lombok.Data;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+
 import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
 
 @Data
 @Entity(name = "tb_tasks")
---
+++ src/main/java/br/com/erickmonteiro/todolist/task/TaskRepository.java
@@ -0,0 +1,8 @@
+package br.com.erickmonteiro.todolist.task;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
